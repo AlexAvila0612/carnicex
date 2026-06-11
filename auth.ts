8742041c500f99d1090359130b5961123666eafd
AlexAvila0612 <alexavila1514@hotmail.com>
2026-06-11 18:58:24 +0200

    Configurar Better Auth y variables de entorno

--- auth.ts
+++ auth.ts
@@ -3,7 +3,7 @@ import { betterAuth } from "better-auth";
 import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
-  database: new Database("database.sqlite"),
+  database: new Database("database.db"),
 
   emailAndPassword: {
     enabled: true,
