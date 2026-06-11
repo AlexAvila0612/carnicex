4674e9340de4f570ecd22abd33933df5a4a7aa5f
AlexAvila0612 <alexavila1514@hotmail.com>
2026-06-11 20:44:51 +0200

    Login, logout y sesiones funcionando

--- app/login/page.js
+++ app/login/page.js
@@ -1,13 +1,105 @@
 "use client";
 
+import { authClient } from "@/auth-client";
+import { useState } from "react";
+
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function registrar() {
+  try {
+    const result = await authClient.signUp.email({
+      email,
+      password,
+      name: email,
+    });
+
+    console.log("REGISTER RESULT:", result);
+    alert("Usuario creado correctamente");
+  } catch (error) {
+    console.error(error);
+    alert("Error al crear usuario");
+  }
+}
+
+  async function login() {
+  try {
+    const result = await authClient.signIn.email({
+      email,
+      password,
+    });
+
+    console.log("LOGIN RESULT:", result);
+    alert("Login correcto");
+  } catch (error) {
+    console.error(error);
+    alert("Error en login");
+  }
+}
+
+async function verSesion() {
+  const res = await fetch("/api/auth/get-session");
+  const data = await res.json();
+
+  console.log(data);
+
+  if (!data) {
+    alert("No autenticado");
+    return;
+  }
+
+  alert(data.user.email);
+}
+
+async function logout() {
+  await authClient.signOut();
+
+  alert("Sesión cerrada");
+}
   return (
     <main style={{ padding: "20px" }}>
       <h1>Login Carnicex</h1>
 
-      <p>Better Auth conectado correctamente.</p>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
 
-      <p>Sesión actual: no autenticado.</p>
+      <br /><br />
+
+      <input
+        type="password"
+        placeholder="Contraseña"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={registrar}>
+        Registrarse
+      </button>
+
+      <br /><br />
+
+      <button onClick={login}>
+        Iniciar sesión
+      </button>
+
+      <br /><br />
+
+<button onClick={verSesion}>
+  Ver sesión
+</button>
+
+      <br /><br />
+
+<button onClick={logout}>
+  Cerrar sesión
+</button>
     </main>
   );
 }
